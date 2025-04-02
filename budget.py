a817a1acaecd5bd1a67b958b5a773cdd48da1b47
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2025-04-01 22:59:26 -0600

    Removed encryption test

--- budget.py
+++ budget.py
@@ -1,28 +1,4 @@
 # Gabriel Crozier Budget Calculator
-from cryptography.fernet import Fernet
 
-print('\033c')
-
-key1 = Fernet.generate_key()
-with open("secret.key", "wb") as key_file:
-    key_file.write(key1)
-
-
-
-with open("secret.key", "rb") as key_file:
-    key = key_file.read()
-f = Fernet(key)
-
-message = "Sensitive data to encrypt"
-encrypted_message = f.encrypt(message.encode())
-
-print("Original message:", message)
-print("Encrypted message:", encrypted_message)
-
-
-with open("secret.key", "rb") as key_file:
-    key = key_file.read()
-f = Fernet(key)
-decrypted_message = f.decrypt(encrypted_message).decode()
-
-print("Decrypted message:", decrypted_message)
+def budget(incomes,expenses,budgets):
+    pass
