9784d6ffdfce96ff24ce65d402238f90e53b453b
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-04-04 16:02:09 -0600

    Created graphs, changed date objects

--- income_expenses.py
+++ income_expenses.py
@@ -40,11 +40,12 @@ def income_entries(income_entry_list):
         income_amount = number_float_input('Please type the amount of money you gained')
         return income_amount
     def get_income_date():
-        income_date = str_input('Please type the date when you got the money (like MM/DD/YY if any of these are single digits type 0, i.e. 10-04-09:')
+        income_date = str_input('Please type the date when you got the money (like MM-DD-YY if any of these are single digits type 0, i.e. 10-04-09:')
         try:
             income_date = parser.parse(income_date)
         except:
             print("Please enter in MM-DD-YY format.")
+            income_date = get_income_date()
         return income_date
     def get_income_source():
         income_source = str_input('Please type where you got the money from:')
@@ -63,11 +64,12 @@ def expense_entries(expense_entry_list):
         income_amount = number_float_input('Please type the amount of money you spent')
         return income_amount
     def get_expense_date():
-        income_date = str_input('Please type the date when you got the money (like MM/DD/YY if any of these are single digits type 0, i.e. 10-04-09:')
+        income_date = str_input('Please type the date when you got the money (like MM-DD-YY if any of these are single digits type 0, i.e. 10-04-09:')
         try:
             income_date = parser.parse(income_date)
         except:
             print("Please enter in MM-DD-YY format.")
+            income_date = get_expense_date()
         return income_date
     def get_expense_category():
         income_source = str_input('Please type what you spent the money on:')
@@ -83,8 +85,15 @@ def expense_entries(expense_entry_list):
 #A function to find and show a specific entry based on date
 def show_income_expense_entry(income_entry_list, expense_entry_list):
     #Inner function for getting the date from the user
+    print(income_entry_list)
+    print(expense_entry_list)
     def get_entry_date():
-        income_entry_date = str_input('Please type the date when you either got the money or spent it (like MM/DD/YY if any of these are single digits type 0(day (less than ten))):')
+        income_entry_date = str_input('Please type the date when you either got the money or spent it (like MM-DD-YY if any of these are single digits type 0(day (less than ten))):')
+        try:
+            income_entry_date = parser.parse(income_entry_date)
+        except:
+            print("Please enter in MM-DD-YY format.")
+            income_entry_date = get_entry_date()
         return income_entry_date
     while True:
         user_input = question(['Income', 'Expense', 'Exit'], 'Choose if you want to see an entry for income or for expense or if you want to exit:')
@@ -95,7 +104,7 @@ def show_income_expense_entry(income_entry_list, expense_entry_list):
                     user_entry_date = get_entry_date()
                     #Finding the income entry and if found showing the income entry
                     for income_entry in income_entry_list:
-                        if income_entry['income_date'] == user_entry_date:
+                        if parser.parse(income_entry['income_date']) == user_entry_date:
                             print("The entry has been found showing entry.")
                             print(f'This is the income amount: {income_entry['income']}')
                             print(f'This is the income date: {income_entry['income_date']}')
@@ -113,7 +122,7 @@ def show_income_expense_entry(income_entry_list, expense_entry_list):
                     user_entry_date = get_entry_date()
                     #Finding the expense entry and if found showing the expense entry
                     for expense_entry in expense_entry_list:
-                        if expense_entry['expense_date'] == user_entry_date:
+                        if parser.parse(expense_entry['expense_date']) == user_entry_date:
                             print("The entry has been found showing entry.")
                             print(f'This is the expense amount: {expense_entry['expense']}')
                             print(f'This is the expense date: {expense_entry['expense_date']}')
